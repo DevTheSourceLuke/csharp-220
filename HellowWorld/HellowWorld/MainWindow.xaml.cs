3796caeda2205c5040c05cf038c7d69367cc5b65
LB <luke429@uw.edu>
2020-04-03 00:04:11 -0700

    Assignment 01 Hello World Updated
    Updated version of HelloWorld WPF Application
    --The uxSubmit button is disabled unless there is text in both uxName and uxPassword.
    --As  soon as the user enter text in uxName and uxPassword, --the uxSubmit button should be enabled.
    
    Scenarios to check:
        Both fields empty -> uxSubmit is disabled
        Only one field has text -> uxSubmit is disabled
        Both fields have text -> uxSubmit is enabled
        Both fields have text then you delete both fields ->     uxSubmit is disabled

---
+++ HellowWorld/HellowWorld/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿// CHSP 220: Assignment 01
+// Luke Bushey
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowState = WindowState.Maximized;
+            uxSubmit.IsEnabled = false;
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password: " + uxPassword.Text);
+        }
+
+        /// <summary>
+        /// Checks whether the password and username field are filled in after either field is changed.
+        /// Will enable the Submit button if both fields are filled in.
+        /// </summary>
+        private void requiredFieldsCheck(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(uxName.Text.ToString()) || string.IsNullOrWhiteSpace(uxPassword.Text.ToString()))
+            {
+                uxSubmit.IsEnabled = false;
+            }
+            else
+            {
+                uxSubmit.IsEnabled = true;
+            }
+        }       
+    }
+}
